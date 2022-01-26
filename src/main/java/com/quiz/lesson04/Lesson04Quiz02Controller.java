5590b654036d1c4fc5cbe95db742a76933c31596
Juhyun-Bak <sihyun2533@gmail.com>
2022-01-26 22:00:53 +0900

    useGeneratedKey #9

--- src/main/java/com/quiz/lesson04/Lesson04Quiz02Controller.java
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz02Controller.java
@@ -3,7 +3,6 @@ package com.quiz.lesson04;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +18,7 @@ public class Lesson04Quiz02Controller {
 	private RealtorBO realtorBO;
 
 	// http://localhost/lesson04/quiz02/1
-	@GetMapping("/1")
+	@RequestMapping("/1")  // 여러 상황을 대비하여 @Post, @Get보다 @Request가 안정적 
 	public String getRealtorView() {
 		return "lesson04/addRealtor";
 	}
@@ -29,14 +28,16 @@ public class Lesson04Quiz02Controller {
 	public String addRealtor(@ModelAttribute Realtor realtor
 			, Model model) {
 		
-		// DB 넣기 
+		// DB insert => id 반환을 받고 
 		realtorBO.addRealtor(realtor);
 		
-		// DB 출력 
-		Realtor result = realtorBO.getRealtorById(realtor.getId());
-		model.addAttribute("result", result);
+		// DB select => 반환받은 id로 model에 담아 return받아 출
+		realtor = realtorBO.getRealtorById(realtor.getId()); // realtor 재활용 
 		
-		return "lesson04/realtorInfo";
+		// jsp에서 사용할 객체를 model에 담는다. 
+		model.addAttribute("result", realtor);
+		
+		return "lesson04/realtorInfo"; // 추가된 공인중개사 jsp
 	}
 	
 }
